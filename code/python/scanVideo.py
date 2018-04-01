826456818d4b6258ea61faf0f7f64e69c7652e18
YongchaoDing <yongchaoding@outlook.com>
2018-04-01 12:44:42 +0800

    Update scanVideo.py

--- code/python/scanVideo.py
+++ code/python/scanVideo.py
@@ -9,11 +9,14 @@ import cv2
 import numpy as np
 import xlrd
 import pandas as pd
+import matplotlib.pyplot as plt
+import math
+from scipy import optimize  
 
 FrameHZ = 30;
 Size = 100
-X_start = 660
-Y_start = 260
+X_start = 900
+Y_start = 300
 
 def  readExcel(file):
     File=xlrd.open_workbook(file)
@@ -30,9 +33,42 @@ def reformForceData(force):
     force_array = [];
     for i in range(len(force)):
         for j in range(FrameHZ):
-            force_array[i*FrameHZ + j] = force[i];
-    return force_array;
+            force_array.append(force[i]);
+    return force_array, len(force_array);
 
+def dataCombine(force_array, b_array, g_array, r_array, force_len, numFrame):
+    dataLen = min(force_len, numFrame);
+    force_array = force_array[:dataLen - 1];
+    b_array = b_array[:dataLen - 1];
+    g_array = g_array[:dataLen - 1];
+    r_array = r_array[:dataLen - 1];
+    CombinedData = [force_array, b_array, g_array, r_array];
+    # print(CombinedData);
+    return CombinedData;
+
+def DrawPlot(DataArray):
+    plt.figure(1);
+    plt.plot(DataArray[0], DataArray[1], '.');
+    
+    plt.figure(2);
+    plt.plot(DataArray[0], DataArray[2], '.');
+    
+    plt.figure(3);
+    plt.plot(DataArray[0], DataArray[3], '.');
+    plt.show();
+
+def f_1(x, A, B):
+    return A*x + B
+
+def curveFit(DataArray):
+    A1, B1 = optimize.curve_fit(f_1, DataArray[0], DataArray[1])[0];
+    plt.figure(1);
+    plt.plot(DataArray[0], DataArray[1], '.');
+    x1 = np.arange(200, 400, 1)  
+    y1 = A1*x1 + B1  
+    plt.plot(x1, y1, "blue") 
+    return A1, B1
+    
 def openVideo(file):
     cap = cv2.VideoCapture(file);
     # shape = (int(VideoCapture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)), int(VideoCapture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
@@ -46,7 +82,7 @@ def openVideo(file):
         Num_frame += 1
     cap.release()
     cap = cv2.VideoCapture(file);
-    return cap, frame, shape
+    return cap, Num_frame, shape
 
 def resizeFrame(frame, shape, coefficient):
     frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
@@ -55,7 +91,7 @@ def resizeFrame(frame, shape, coefficient):
 
 def showFrame(name, frame):
     cv2.imshow(name, frame);
-    keycode = cv2.waitKey(20);
+    cv2.waitKey(20);
     #if(keycode == 27):
     #   break;
 
@@ -93,18 +129,17 @@ def rgbDivide(frame):
     b = cv2.split(frame)[0];
     g = cv2.split(frame)[1];
     r = cv2.split(frame)[2];
-    b_sum = b.mean() * Size * Size;
-    g_sum = g.mean() * Size * Size;
-    r_sum = r.mean() * Size * Size;
+    b_sum = math.log(b.mean()) * Size * Size;
+    g_sum = math.log(g.mean()) * Size * Size;
+    r_sum = math.log(r.mean()) * Size * Size;
     return b_sum, g_sum, r_sum
 
 if __name__ == '__main__':
     # read video and excel file
     _, force = readExcel('../../ForceData/ForceTest.xlsx');
-    force_array = reformForceData(force);
+    force_array, force_len = reformForceData(force);
     cap, numFrame, shape = openVideo('../../VideoData/VideoTest.MOV');
     # init some params
-    print(force[0]);
     frame_num= 0;
     b_array = [];
     g_array = [];
@@ -112,7 +147,7 @@ if __name__ == '__main__':
     while(cap.isOpened()):
         frame_num = frame_num + 1;
         ret, frame  =cap.read()
-        if ret == False:
+        if ret == False:# or frame_num > 100:
             break;
         originImage = frame.copy()
         frame = resizeFrame(frame, shape, 0.5)
@@ -131,8 +166,14 @@ if __name__ == '__main__':
         #showFrame('Frame', frame);
         #showFrame('ROI', imageROI);
 
+    CombinedData = dataCombine(force_array, b_array, g_array, r_array, force_len, frame_num);
+    DataArray=np.array(CombinedData);
+    DataArray.sort();
+    curveFit(DataArray);
+    #DrawPlot(DataArray);
+    
     print("len of force is %d, rgb is %d\n" %(len(force_array), len(b_array)));
-    dataframe = pd.DataFrame({'b':b_array,'g':g_array,'r':r_array});
+    dataframe = pd.DataFrame({'force':CombinedData[0],'b':CombinedData[1],'g':CombinedData[2],'r':CombinedData[3]});
     dataframe.to_csv("VideoTest.csv",index=False,sep=',');
 
 
